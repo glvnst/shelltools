7b26003273725fff5a7394b988d5db0569e2f4dd
glvnst <benjaminburke@me.com>
2013-05-24 13:57:33 -0700

    initial commit of collect.sh

---
+++ collect.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+COLLECT_LAST=""
+
+collect () 
+{ 
+    # Check argument count, show help if incorrect
+    if ((${#@} < 2)); then
+        echo "Usage: collect <directory> <file to mv to directory> ..." 1>&2;
+        echo "  * The directory will be created if it doesn't exist." 1>&2;
+        return 1;
+    fi;
+
+    local directory=$1;
+    shift;
+    
+    # Make the directory if necessary
+    if [ '!' -d "$directory" ]; then
+        mkdir -p "$directory";
+    fi;
+
+    # Do the move
+    mv -i -- "$@" "$directory/";
+    COLLECT_LAST="$directory";
+
+    return 0
+}
+export -f collect
+
+
+collectd () 
+{ 
+    # Check argument count, show help if incorrect
+    if ((${#@} < 2)); then
+        echo "Usage: collect <directory> <file to mv to directory> ..." 1>&2;
+        echo "  * The directory will be created if it doesn't exist." 1>&2;
+        echo "  * After the mv, 'pushd <directory>' will be called." 1>&2;
+        return 1;
+    fi;
+
+    # collect, then cd to created directory
+    collect "$@";
+    pushd "$COLLECT_LAST"
+}
+export -f collectd

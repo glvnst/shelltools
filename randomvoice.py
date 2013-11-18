92ddeafe56babb1ab1a77717e1d22b62d9f77f1f
glvnst <benjaminburke@me.com>
2013-11-18 12:33:26 -0800

    pep8 adjustments

--- randomvoice.py
+++ randomvoice.py
@@ -4,9 +4,10 @@ import subprocess
 import random
 import argparse
 
-NOVELTY_VOICES=set(['Albert', 'Bad News', 'Bahh', 'Bells', 'Boing', 'Bubbles',
-'Cellos', 'Deranged', 'Good News', 'Hysterical', 'Pipe Organ', 'Trinoids',
-'Whisper', 'Zarvox'])
+NOVELTY_VOICES = ['Albert', 'Bad News', 'Bahh', 'Bells', 'Boing', 'Bubbles',
+                  'Cellos', 'Deranged', 'Good News', 'Hysterical',
+                  'Pipe Organ', 'Trinoids', 'Whisper', 'Zarvox']
+
 
 def get_voice_list():
     """ Return a list of available voices on the system """
@@ -17,18 +18,19 @@ def get_voice_list():
     return voices
 
 if __name__ == '__main__':
-    ARGP = argparse.ArgumentParser(description=("Print the randomly-chosen "
-        "name of an available text-to-speech voice"))
+    ARGP = argparse.ArgumentParser(
+        description=("Print the randomly-chosen name of an available "
+                     "text-to-speech voice"))
     ARGP.add_argument("-e", "--exclude-novelty-voices", action="store_true",
-        help="exclude the novelty voice set")
+                      help="exclude the novelty voice set")
     ARGP.add_argument("-n", "--only-novelty-voices", action="store_true",
-        help="only choose from the novelty voice set")
+                      help="only choose from the novelty voice set")
     ARGS = ARGP.parse_args()
 
     VOICES = get_voice_list()
     if ARGS.exclude_novelty_voices:
-        VOICES=list(set(VOICES) - NOVELTY_VOICES)
+        VOICES = list(set(VOICES) - set(NOVELTY_VOICES))
     if ARGS.only_novelty_voices:
-        VOICES=list(set(VOICES) & NOVELTY_VOICES)
+        VOICES = list(set(VOICES) & set(NOVELTY_VOICES))
 
     print random.choice(VOICES)
--- randrange.py
+++ randrange.py
@@ -5,21 +5,20 @@ import random
 import sys
 
 if __name__ == "__main__":
-    ARGP = argparse.ArgumentParser(description=("Print a random value using "
-        "the specified parameters"))
+    ARGP = argparse.ArgumentParser(
+        description="Print a random value using the specified parameters")
     ARGP.add_argument("low", type=int,
-        help="the lower boundary for the allowable range")
+                      help="the lower boundary for the allowable range")
     ARGP.add_argument("high", type=int,
-        help="the upper boundary for the allowable range")
+                      help="the upper boundary for the allowable range")
     ARGP.add_argument("-c", "--count", type=int, default=1,
-        help="the number of random values to print")
+                      help="the number of random values to print")
     ARGS = ARGP.parse_args()
 
     if ARGS.low >= ARGS.high:
         sys.stderr.write("Invalid range specified: {}-{}\n".format(ARGS.low,
                                                                    ARGS.high))
         sys.exit(1)
-
 
     for x in xrange(ARGS.count):
         print random.randrange(start=ARGS.low, stop=ARGS.high)

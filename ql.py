22efdc49ba150c0355fcad79203249c199eac963
glvnst <glvnst@users.noreply.github.com>
2020-03-18 10:12:06 +0100

    bringing repo up to date with local changes

--- ql.py
+++ ql.py
@@ -1,86 +1,133 @@
 #!/usr/bin/python
-""" command-line OS X quicklook tool
-Apple's docs on Uniform Type Identifiers can be found on their website at:
-http://tinyurl.com/utiref
-"""
-from __future__ import print_function
+""" invoke macOS quicklook feature from the command-line """
 import argparse
-import subprocess
 import os
+import subprocess
 import sys
 
-SIMPLE_TYPES = {'text': 'public.text',
-                'image': 'public.image',
-                'movie': 'public.movie',
-                'audio': 'public.audio',
-                'sound': 'public.audio'}
+# Apple's docs on Uniform Type Identifiers can be found on their website at:
+# https://developer.apple.com/library/archive/documentation/Miscellaneous/Reference/UTIRef/Articles/System-DeclaredUniformTypeIdentifiers.html#//apple_ref/doc/uid/TP40009259-SW1
 
-SPECIAL_NAMES = {'README': 'public.text',
-                 'LICENSE': 'public.text',
-                 'Makefile': 'public.source-code',
-                 'conf': 'public.source-code'}
+SIMPLE_TYPES = {
+    'text': 'public.plain-text',
+    'image': 'public.image',
+    'movie': 'public.movie',
+    'audio': 'public.audio',
+    'sound': 'public.audio'
+}
+
+SPECIAL_NAMES = {
+    'README': 'public.plain-text',
+    'LICENSE': 'public.plain-text',
+    'Makefile': 'public.source-code',
+    'conf': 'public.source-code'
+}
+
+SPECIAL_EXTENSIONS = {
+    '.md': 'public.plain-text'
+}
 
 
-def warning(*objs):
+def warn(message):
     """ Write a warning message to stderr """
-    print("WARNING:", *objs, file=sys.stderr)
+    sys.stderr.write("WARN: {}\n".format(message))
 
 
-def quicklook(document_list, uniform_type=None):
+def quicklook(documents, uniform_type=None, debug=False):
     """
     Open a quicklook preview of the given documents with the optionally
     specified type
     """
     # reconstruct the list, looking for things that don't exist and giving
     # appropriate warnings
-    final_document_list = list()
-    for document in document_list:
+    filtered_documents = list()
+    for document in documents:
         if not os.path.exists(document):
-            warning('Could not find a file named "{}"'.format(document))
+            warn('Could not find a file named "{}"'.format(document))
             continue
-        final_document_list.append(document)
-
-    if not final_document_list:
-        warning("No documents to open")
-        return
+        filtered_documents.append(document)
+    if not filtered_documents:
+        warn("No documents to open")
+        return None
 
     command = ['qlmanage']
-
     if uniform_type:
         command.extend(['-c', uniform_type])
-
-    command.append('-p')
-    command.extend(final_document_list)
+    command.extend(['-p'] + filtered_documents)
 
     with open(os.devnull, 'wb') as devnull:
-        subprocess.call(command, stdout=devnull, stderr=subprocess.STDOUT)
+        if debug:
+            stdout = sys.stdout
+            stderr = sys.stderr
+        else:
+            stdout = devnull
+            stderr = subprocess.STDOUT
+        return subprocess.check_call(command, stdout=stdout, stderr=stderr)
+
+
+def fileext(filename):
+    """ return the filename extension for the given filename """
+    parts = os.path.splitext(filename)
+    if len(parts) < 2:
+        return filename
+    return parts[1]
+
+
+def resolve_types_set(paths):
+    """ return a set of the computed uniform types for the given paths """
+    types = list()
+    for path in paths:
+        filename = os.path.basename(path)
+        if filename in SPECIAL_NAMES:
+            types.append(SPECIAL_NAMES[filename])
+            continue
+        ext = fileext(filename)
+        if ext in SPECIAL_EXTENSIONS:
+            types.append(SPECIAL_EXTENSIONS[ext])
+            continue
+    return set(types)
+
+
+def main():
+    """ handler for command-line use """
+    argp = argparse.ArgumentParser(
+        description=("command-line quicklook previewer"))
+    argp.add_argument("-d", "--debug", action="store_true", help=(
+        "enable qlmanage's console output"))
+    argp.add_argument("documents", type=str, nargs="+", help=(
+        "one or more documents to preview"))
+    # mutually exclusive argument group: -t and -u
+    argp_meg_type = argp.add_mutually_exclusive_group(required=False)
+    argp_meg_type.add_argument("-t", "--type", type=str, help=(
+        "specifies the simplified type of the given document(s)"))
+    argp_meg_type.add_argument("-u", "--uniformtype", type=str, help=(
+        "specifies the Uniform Type Identifier of the given document(s)"))
+    #
+    args = argp.parse_args()
+
+    uniform_type = None
+
+    if args.uniformtype:
+        uniform_type = args.uniformtype
+    elif args.type:
+        uniform_type = SIMPLE_TYPES.get(args.type, "public." + args.type)
+    else:
+        # unfortunately qlmanage only accepts a single uniform type code --
+        # you can't specify "-c" multiple times.
+        #
+        # so we try to compute the type of all the files we are given
+        # and if there is only one type for the whole bunch -- we use that,
+        # otherwise we don't use the computed types at all
+        #
+        # I considered lanching qlmanage multiple times (once per uniform type)
+        # but the UX is terrible, particularly if you make a mistake and give
+        # a long list of differently-typed files to preview
+        types = resolve_types_set(args.documents)
+        if len(types) == 1:
+            uniform_type = list(types)[0]
+
+    quicklook(args.documents, uniform_type=uniform_type, debug=args.debug)
 
 
 if __name__ == "__main__":
-    ARGP = argparse.ArgumentParser(description=("command-line interface to "
-                                                "quicklook"))
-    ARGP.add_argument("documents", type=str, nargs="+",
-                      help="the documents to preview")
-    ARGP_MEG_TYPE = ARGP.add_mutually_exclusive_group(required=False)
-    ARGP_MEG_TYPE.add_argument("-t", "--type", type=str,
-                               help=("specifies the simplified type of the "
-                                     "given document(s)"))
-    ARGP_MEG_TYPE.add_argument("-u", "--uti", type=str,
-                               help=("specifies the Uniform Type Identifier "
-                                     "of the given document(s)"))
-    ARGS = ARGP.parse_args()
-
-    UTI = None
-
-    if ARGS.uti:
-        UTI = ARGS.uti
-    elif ARGS.type:
-        UTI = SIMPLE_TYPES.get(ARGS.type, "public.{}".format(ARGS.type))
-    else:
-        SPECIAL_DOCS = list(set(SPECIAL_NAMES.keys()) &
-                            set([os.path.basename(path)
-                                 for path in ARGS.documents]))
-        if SPECIAL_DOCS:
-            UTI = SPECIAL_NAMES[SPECIAL_DOCS[0]]
-
-    quicklook(ARGS.documents, uniform_type=UTI)
+    main()

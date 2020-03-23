5fd27cc62570cb0e96ab935b55f931c8ae6dc606
glvnst <glvnst@users.noreply.github.com>
2020-03-23 13:59:13 +0100

    reformatting

--- fmt_duration_test.sh
+++ fmt_duration_test.sh
@@ -4,59 +4,81 @@ testShellCheck() {
   shellcheck "${TESTING_TARGET}" || fail "shellcheck failed"
 }
 
-testImportMode() {
-  output=$(sh -c "export IMPORT=1; . ${TESTING_TARGET}")
+testUsage() {
+  # no args, -h and --help invocations should contain the word usage
 
-  assertEquals "Import mode should output nothing" "$output" ""
+  assertContains \
+    "invocation without arguments should output usage" \
+    "$( sh -c "${TESTING_TARGET} 2>&1" )" \
+    "Usage:"
+
+  assertContains \
+    "invocation without arguments should output usage" \
+    "$( sh -c "${TESTING_TARGET} -h 2>&1" )" \
+    "Usage:"
+
+  assertContains \
+    "invocation without arguments should output usage" \
+    "$( sh -c "${TESTING_TARGET} --help 2>&1" )" \
+    "Usage:"
 }
 
-testFmtDuration() {
-  assertEquals "input 0" "$(fmt_duration 0)" "0 seconds"
-  assertEquals "input 1" "$(fmt_duration 1)" "1 second"
-  assertEquals "input 2" "$(fmt_duration 2)" "2 seconds"
+testImportMode() {
+  assertEquals \
+    "Import mode should output nothing" \
+    "$(sh -c "export IMPORT=1; . ${TESTING_TARGET}")" \
+    ""
+}
+
+testFmtDurationInputs() {
+  assertEquals \
+    "zero; 0 = 0 seconds" \
+    "$(fmt_duration 0)" \
+    "0 seconds"
 
   assertEquals \
-    "input 1d-1s" \
+    "singular; 1 = 1 second" \
+    "$(fmt_duration 1)" \
+    "1 second"
+
+  assertEquals \
+    "plural; 2 = 2 seconds" \
+    "$(fmt_duration 2)" \
+    "2 seconds"
+
+  assertEquals \
+    "multiple clauses; input 1d-1s" \
     "$(fmt_duration 86399)" \
     "23 hours, 59 minutes, 59 seconds"
 
   assertEquals \
-    "input 1d" \
+    "non-terminal singualr; input 1d" \
     "$(fmt_duration 86400)" \
     "1 day"
 
   assertEquals \
-    "input 1d+2s" \
-    "$(fmt_duration 86402)" \
-    "1 day, 2 seconds"
-
-  assertEquals \
-    "input 60*60*24*365.25" \
+    "1y==365.25, input 365d" \
     "$(fmt_duration 31536000)" \
     "365 days"
 
   assertEquals \
-    "input 365d + 6h - 1s" \
-    "$(fmt_duration 31557599)" \
-    "365 days, 5 hours, 59 minutes, 59 seconds"
-
-  assertEquals \
-    "input 365d + 6h" \
+    "1y=365.25; input 365.25d" \
     "$(fmt_duration 31557600)" \
     "1 year"
 
   assertEquals \
-    "input 365d in s" \
+    "only seconds; 365d in s" \
     "$(fmt_duration 31536000 second/seconds:1)" \
     "31536000 seconds"
 
   assertEquals \
-    "input 365.25d in w, h, s" \
+    "365.25d in w, h, s" \
     "$(fmt_duration 31557601 week/weeks:604800 hour/hours:3600 second/seconds:1)" \
     "52 weeks, 30 hours, 1 second"
 }
 
 oneTimeSetUp() {
+  # shellcheck source=/dev/null
   IMPORT=1 . "${TESTING_TARGET}"
 }
 

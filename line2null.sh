7ecdbd8d84b43bb54504b2fe9fd8c01524e9694c
glvnst <glvnst@users.noreply.github.com>
2020-03-23 14:40:58 +0100

    Adding support for usage text and making it importable

--- line2null.sh
+++ line2null.sh
@@ -2,5 +2,28 @@
 # converts newlines to nulls, useful with xargs -0
 # installed_name:line2null
 
-exec tr '\n' '\0'
+usage() {
+  self="$(basename "$0")"
 
+  printf '%s\n' \
+    "Usage: $self [-h|--help]" \
+    "" \
+    "This utility converts linefeeds on the standard input into null" \
+    "characters. This can be useful in combination with the xargs -0" \
+    "facility." \
+    ""
+
+  exit 1
+}
+
+line2null() {
+  exec tr '\n' '\0'
+}
+
+main() {
+  [ -z "$*" ] || usage
+
+  line2null
+}
+
+[ -n "$IMPORT" ] || main "$@"

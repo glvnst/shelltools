6a52651642510180afdcb3f285627e81d443b22a
glvnst <glvnst@users.noreply.github.com>
2020-04-02 14:17:32 +0200

    Restructuring into got/want variable checks. Did some testing with kcov

--- fmt_duration/fmt_duration_test.sh
+++ fmt_duration/fmt_duration_test.sh
@@ -1,86 +1,96 @@
 #!/bin/sh
 
+TARGET="./fmt_duration.sh"
+
 testShellCheck() {
-  shellcheck "${TESTING_TARGET}" || fail "shellcheck failed"
-}
-
-testUsage() {
-  # no args, -h and --help invocations should contain the word usage
-
-  assertContains \
-    "invocation without arguments should output usage" \
-    "$( sh -c "${TESTING_TARGET} 2>&1" )" \
-    "Usage:"
-
-  assertContains \
-    "invocation without arguments should output usage" \
-    "$( sh -c "${TESTING_TARGET} -h 2>&1" )" \
-    "Usage:"
-
-  assertContains \
-    "invocation without arguments should output usage" \
-    "$( sh -c "${TESTING_TARGET} --help 2>&1" )" \
-    "Usage:"
+  shellcheck "${TARGET}" || fail "shellcheck failed"
 }
 
 testImportMode() {
-  assertEquals \
-    "Import mode should output nothing" \
-    "$(sh -c "export IMPORT=1; . ${TESTING_TARGET}")" \
-    ""
+  got="$(sh -c "export IMPORT=1; . ${TARGET}")"
+  want=""
+  assertEquals "Import mode should output nothing" "$want" "$got"
 }
 
-testFmtDurationInputs() {
-  assertEquals \
-    "zero; 0 = 0 seconds" \
-    "$(fmt_duration 0)" \
-    "0 seconds"
+testWarn() {
+  output="$( warn testWarn 2>&1 >/dev/null | grep -E '^....-..-..T..:..:.. testWarn$' )"
+  contains="testWarn"
+  assertContains "test warning function output" "$output" "$contains"
+}
 
-  assertEquals \
-    "singular; 1 = 1 second" \
-    "$(fmt_duration 1)" \
-    "1 second"
+testDie() {
+  output="$( die testDie 2>&1 >/dev/null | grep -E '^....-..-..T..:..:.. testDie' )"
+  contains="testDie"
+  assertContains "test die function output" "$output" "$contains"
+  contains="EXITING"
+  assertContains "test die function output" "$output" "$contains"
 
-  assertEquals \
-    "plural; 2 = 2 seconds" \
-    "$(fmt_duration 2)" \
-    "2 seconds"
+  ( die "testDie2" >/dev/null 2>&1 )
+  got=$?
+  want=1
+  assertEquals "die exit code should be 1" "$want" "$got"
+}
 
-  assertEquals \
-    "multiple clauses; input 1d-1s" \
-    "$(fmt_duration 86399)" \
-    "23 hours, 59 minutes, 59 seconds"
+testMain() {
+  got="$(main 167212082)"
+  want="5 years, 109 days, 1 hour, 48 minutes, 2 seconds"
+  assertEquals "invoke with valid argument" "$want" "$got"
 
-  assertEquals \
-    "non-terminal singualr; input 1d" \
-    "$(fmt_duration 86400)" \
-    "1 day"
+  output="$( main )"
+  contains="Usage:"
+  assertContains "invoke without args = show usage" "$output" "$contains"
 
-  assertEquals \
-    "1y==365.25, input 365d" \
-    "$(fmt_duration 31536000)" \
-    "365 days"
+  output="$( main -h 2>&1 )"
+  contains="Usage:"
+  assertContains "invoke with -h = show usage" "$output" "$contains"
 
-  assertEquals \
-    "1y=365.25; input 365.25d" \
-    "$(fmt_duration 31557600)" \
-    "1 year"
+  output="$( main --help 2>&1 )"
+  contains="Usage:"
+  assertContains "invoke with --help = show usage" "$output" "$contains"
+}
 
-  assertEquals \
-    "only seconds; 365d in s" \
-    "$(fmt_duration 31536000 second/seconds:1)" \
-    "31536000 seconds"
+testFmtDuration() {
+  got="$(fmt_duration 0)"
+  want="0 seconds"
+  assertEquals "zero; 0 = 0 seconds" "$want" "$got"
 
-  assertEquals \
-    "365.25d in w, h, s" \
-    "$(fmt_duration 31557601 week/weeks:604800 hour/hours:3600 second/seconds:1)" \
-    "52 weeks, 30 hours, 1 second"
+  got="$(fmt_duration 1)"
+  want="1 second"
+  assertEquals "singular; 1 = 1 second" "$want" "$got"
+
+  got="$(fmt_duration 2)"
+  want="2 seconds"
+  assertEquals "plural; 2 = 2 seconds" "$want" "$got"
+
+  got="$(fmt_duration 86399)"
+  want="23 hours, 59 minutes, 59 seconds"
+  assertEquals "multiple clauses; input 1d-1s" "$want" "$got"
+
+  got="$(fmt_duration 86400)"
+  want="1 day"
+  assertEquals "non-terminal singualr; input 1d" "$want" "$got"
+
+  got="$(fmt_duration 31536000)"
+  want="365 days"
+  assertEquals "1y==365.25, input 365d" "$want" "$got"
+
+  got="$(fmt_duration 31557600)"
+  want="1 year"
+  assertEquals "1y=365.25; input 365.25d" "$want" "$got"
+
+  got="$(fmt_duration 31536000 second/seconds:1)"
+  want="31536000 seconds"
+  assertEquals "only seconds; 365d in s" "$want" "$got"
+
+  got="$(fmt_duration 31557601 week/weeks:604800 hour/hours:3600 second/seconds:1)"
+  want="52 weeks, 30 hours, 1 second"
+  assertEquals "365.25d in w, h, s" "$want" "$got"
 }
 
 oneTimeSetUp() {
   # shellcheck source=/dev/null
-  IMPORT=1 . "${TESTING_TARGET}"
+  IMPORT=1 . ${TARGET}
 }
 
 # shellcheck source=/dev/null
-. "$SHUNIT_PATH"
+. "${SHUNIT_PATH:-./shunit2}"

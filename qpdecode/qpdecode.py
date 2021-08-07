f053c45613f2eacc3c161ff7bc8e66901944ed50
glvnst <glvnst@users.noreply.github.com>
2021-08-07 13:04:46 +0200

    bugfixes, support encoding and header arguments

--- qpdecode/qpdecode.py
+++ qpdecode/qpdecode.py
@@ -5,38 +5,49 @@ import quopri
 import sys
 
 
-def write_decoded(input_string):
-    """ decode the given input string and write the result to STDOUT """
-    sys.stdout.write(quopri.decodestring(input_string))
+def qpdecoded(
+    input_bytes: bytes, encoding: str = "iso-8859-1", header: bool = False
+) -> str:
+    """
+    decode the given input string and write the result to STDOUT
+    """
+    return quopri.decodestring(input_bytes, header=header).decode(encoding)
 
 
-def main():
-    """ entry point for command-line execution """
+def main() -> int:
+    """
+    entry point for command-line execution; return value suitable for use with sys.exit
+    """
     argp = argparse.ArgumentParser(
         description="This command performs MIME quoted-printable transport decoding"
     )
-    argp_meg = argp.add_mutually_exclusive_group(required=True)
-    argp_meg.add_argument(
-        "-f",
-        "--file",
-        type=argparse.FileType("r"),
+    argp.add_argument(
+        "file",
+        type=argparse.FileType("rb"),
+        nargs="+",
         help="a file path which should be read and decoded to the standard out",
     )
-    argp_meg.add_argument(
-        "-s", "--string", type=str, help="a string to be decoded to standard out"
+    argp.add_argument(
+        "-e",
+        "--encoding",
+        default="iso-8859-1",
+        help="specify the encoding of the input file(s)",
+    )
+    argp.add_argument(
+        "--header",
+        "--qencoded",
+        action="store_true",
+        help="treat the input as a Q-encoded header (RFC 1522, Part Two) wherein underscore maps to space",
     )
     args = argp.parse_args()
 
-    input_data = ""
-    if args.file:
-        for filehandle in args.file:
-            input_data = filehandle.read()
-    else:
-        input_data = args.string
+    for filehandle in args.file:
+        input_data = filehandle.read()
+        decoded_data = qpdecoded(input_data, encoding=args.encoding, header=args.header)
+        sys.stdout.write(decoded_data + "\n")
 
-    if input_data:
-        write_decoded(input_data + "\n")
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
